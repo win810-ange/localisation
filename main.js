51ad0ceb81d9049fcb957a4b427ba467c001aaf0
win810-ange <angesakaw@gmail.com>
2026-04-14 00:21:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let btnlocalisation = document.getElementById('btnlocalisation')
 btnlocalisation.addEventListener('click', ()=>{
     let datalocalisation = navigator.geolocation.getCurrentPosition()
     let data = datalocalisation.json()
-    window.open(`https//:wa.me/243810340232/${data}`)
+    let zone = document.getElementById("zone")
+    zone.innerHTML  = data
     
 })
